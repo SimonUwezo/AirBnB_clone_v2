73fd867958c45fc49ea7b235ba74951ab2034dc1
SimonUwezo <simonnmwangi@gmail.com>
2024-05-17 22:00:17 -0700

    Readme

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
-
+""" Starts a Flask web application """
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+@app.route('/')
+def hello_hbnb():
+    """ Function that sys Hello Hbnb"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """returns HBNB"""
+    """ Function that says hbnb """
     return 'HBNB'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
+    app.url_map.strict_slashes = False
