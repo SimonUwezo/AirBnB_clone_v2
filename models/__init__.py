45da981d8cd5f2f75dfba306c2ba98a371511a95
SimonUwezo <simonnmwangi@gmail.com>
2024-05-19 04:56:30 -0700

    init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-initialize the models package
+Initialize the models package
 """
 
 from os import getenv
 
-
+# Retrieve the storage type from environment variables
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
+# Depending on the storage type, import and initialize the appropriate storage engine
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
+# Reload the storage to load any existing data
 storage.reload()
